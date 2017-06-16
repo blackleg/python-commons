d03fa86e7260c484565426fb156fc72132080888
blackleg <hectorespertpardo@gmail.com>
2017-06-16 13:10:32 +0200

    Add LICENCE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='blackleg',
+    version='1.0.0',
+    packages=['tests', 'blackleg'],
+    url='',
+    license='',
+    author='blackleg',
+    author_email='hectorespertpardo@gmail.com',
+    description=''
+)
